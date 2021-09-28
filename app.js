19e89378f4bfa8bd19792ed68732ae63e5505b17
RayceWheat <RayceWheat@gmail.com>
2021-09-28 16:18:28 -0500

    initial user input with inqueirer npm

--- app.js
+++ app.js
@@ -1,10 +1,21 @@
+const inquirer = require('inquirer');
+
+inquirer
+  .prompt([
+      {
+          type: 'input',
+          name: 'name',
+          message: 'What is your name?'
+      }
+  ])
+  .then(answers => console.log(answers));
+
+/*
 const fs = require('fs');
 
 const generatePage = require('./src/page-template.js');
 
-const profileDataArgs = process.argv.slice(2);
-
-const [name, github] = profileDataArgs;
+const pageHTML = generatePage(name, github);
 
 
 fs.writeFile('./index.html', generatePage(name, github), err => {
@@ -12,3 +23,4 @@ fs.writeFile('./index.html', generatePage(name, github), err => {
 
     console.log('Portfolio complete! Check out index.html to see the output!')
 });
+*/
