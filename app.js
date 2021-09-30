5cbad2e06a9915154fdc00c78be1fe16708dc458
RayceWheat <RayceWheat@gmail.com>
2021-09-29 20:09:12 -0500

    html functionality

--- app.js
+++ app.js
@@ -1,5 +1,10 @@
 const inquirer = require('inquirer');
 
+const fs = require('fs');
+
+const generatePage = require('./src/page-template.js');
+
+
 const promptUser = () => {
   return inquirer.prompt([
       {
@@ -132,14 +137,17 @@ const promptProject = portfolioData => {
     promptUser()
     .then(promptProject)
     .then(portfolioData => {
-        console.log(portfolioData);
+          const pageHTML = generatePage(portfolioData);
+  
+         fs.writeFile('./index.html', pageHTML, err => {
+           if (err) throw new Error(err);
+  
+         });
     });
     
+
+
 /*
-const fs = require('fs');
-
-const generatePage = require('./src/page-template.js');
-
 const pageHTML = generatePage(name, github);
 
 
